614e55f98d9506f908caf33b8a0ab3493b5d51ce
Luis Carlin <luiscarlin@gmail.com>
2023-09-21 17:36:09 -0400

    add more content to the credit card page

--- components/paymentInfoView.tsx
+++ components/paymentInfoView.tsx
@@ -28,6 +28,8 @@ function PaymentInfoView({ onBookClick }: PaymentInfoViewProps) {
       left={<InfoCard />}
       right={
         <FormCard title="Secure your appointment by card">
+          <p>A credit or debit card is required to book your appointment.</p>
+
           <Label>Card Information</Label>
           <div>
             <InputField
@@ -38,23 +40,25 @@ function PaymentInfoView({ onBookClick }: PaymentInfoViewProps) {
               <ErrorMessage>card number is required</ErrorMessage>
             )}
           </div>
-          <div>
-            <InputField
-              placeholder="MM / YY"
-              {...register("cardExpiry", { required: true })}
-            />
-            {errors.cardExpiry && (
-              <ErrorMessage>card expiration is required</ErrorMessage>
-            )}
-          </div>
-          <div>
-            <InputField
-              placeholder="CVV"
-              {...register("cardCvv", { required: true })}
-            />
-            {errors.cardCvv && (
-              <ErrorMessage>card cvv is required</ErrorMessage>
-            )}
+          <div style={{ display: "flex", gap: "16px" }}>
+            <div style={{ flex: 1 }}>
+              <InputField
+                placeholder="MM / YY"
+                {...register("cardExpiry", { required: true })}
+              />
+              {errors.cardExpiry && (
+                <ErrorMessage>card expiration is required</ErrorMessage>
+              )}
+            </div>
+            <div style={{ flex: 1 }}>
+              <InputField
+                placeholder="CVV"
+                {...register("cardCvv", { required: true })}
+              />
+              {errors.cardCvv && (
+                <ErrorMessage>card cvv is required</ErrorMessage>
+              )}
+            </div>
           </div>
           <div>
             <InputField
@@ -66,14 +70,29 @@ function PaymentInfoView({ onBookClick }: PaymentInfoViewProps) {
             )}
           </div>
           <div>
-            <input
-              type="checkbox"
-              {...register("cancellationFeeAgreed", { required: true })}
-            />
+            <div
+              style={{ display: "flex", gap: "16px", alignItems: "flex-start" }}
+            >
+              <input
+                type="checkbox"
+                {...register("cancellationFeeAgreed", { required: true })}
+              />
+              <p>
+                We ask that you please reschedule or cancel at least 24 hours
+                before the beginning of your appointment or you may be charged a
+                cancellation fee of $50. In the event of emergency, contact us
+                directly. Your card will held in case of late cancellation and
+                for future purchases. It will not be charged now.
+              </p>
+            </div>
             {errors.cancellationFeeAgreed && (
               <ErrorMessage>You have to agree</ErrorMessage>
             )}
           </div>
+          <p>
+            By creating this appointment, you acknowledge you will receive
+            automated transactional messages from this merchant.
+          </p>
         </FormCard>
       }
       footer={
